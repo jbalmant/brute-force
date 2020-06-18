77b0d31dd8e496ceef7c7f1429d032f9ab4d73f3
jonatas <jonatas.balmant@tivit.com>
2020-06-17 21:58:16 -0300

    prod

--- script.py
+++ script.py
@@ -1,57 +1,47 @@
 import logging
 import subprocess
 import time
-from concurrent.futures import ProcessPoolExecutor
-from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
-# from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 
 logging.basicConfig(filename='c:/brute-force.log', format='%(asctime)s %(message)s', level=logging.DEBUG)
 
-CORES = 1
-MAX = 100
+CORES = 127
+START = 1
+END = 100000000
+VOLUME = "E:"
 
-values = range(1, MAX, 1)
+values = range(START, END, 1)
 
 wait = False
 
 
-def mount(passwd, volume="D:", path="C:/Program Files (x86)/Jetico/BestCrypt Volume Encryption"):
+def mount(passwd, volume=VOLUME, path="C:/Program Files (x86)/Jetico/BestCrypt Volume Encryption"):
     password = '{:0>8d}'.format(passwd)
     try:
-        # while (wait):
-        #     pass
-        p = subprocess.Popen([path + "/bcfmgr.exe", "-Mount", volume, "-P" + password], shell=False)
-        # p.communicate()
+        p = subprocess.Popen([path + "/bcfmgr.exe", "-Mount", volume, "-P" + password],
+                             shell=False,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        p.communicate()
+
+        #logging.error(f'err {stderr.decode("utf-8")} out {stdout.decode("utf-8")} rcode {p.returncode}')
     except:
-        logging.error(f'retry {password}')
+        logging.error(f'exception retry {password}')
     return password
 
 
 def main():
     start_time = time.time()
 
-    print(f"CPU {CORES} - MAX {MAX} - {datetime.utcnow()}")
+    print(f"VOLUME {VOLUME} CPU {CORES} - START {START} - END {END} - {datetime.utcnow()}")
 
-    # for i in values:
-    #     mount(i)
-    #
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    #
-    # return
-
-    with ProcessPoolExecutor(max_workers=CORES) as executor:
+    with ThreadPoolExecutor(max_workers=CORES) as executor:
         results = executor.map(mount, values)
         for result in results:
-            if int(result) % (CORES * 10) == 0:
+            if int(result) % (1000) == 0:
                 logging.debug(result)
-
-                # print('Calm down')
-                #
-                # global wait
-                # wait = True
-                # time.sleep(2)
-                # wait = False
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
